0f399edcfe46c2f10b5348bad726fca82a491f69
BuildTools <unconfigured@null.spigotmc.org>
2026-04-07 13:25:04 -0400

    added host firewall for each server

--- scripts/firewall/db-firewall.sh
+++ scripts/firewall/db-firewall.sh
@@ -3,22 +3,20 @@
 # This script will avoid using aliases
 # Script must be run as root
 if [ "$EUID" -ne 0 ]; then 
-  echo "Rquires Root: sudo ./shell-firewall.sh"
+  echo "Rquires Root: sudo ./db-firewall.sh"
   exit
 fi
 
 # Variables
 INTERFACE="en0" # Replace with Interface of server
-SSH_SCORING_IP="0.0.0.0" # Replace with SSH scoring IP if found
-SMB_SCORING_IP="0.0.0.0" # Replace with SMB socring IP if found
+DB_SCORING_IP="0.0.0.0" # Replace with DB scoring IP if found
 EXTERNAL_KALI="192.168.24.20" # Replace with IP of any Kali machine used for remote access
-DHCP_SERVER="0.0.0.0" # Replace with DHCP Server IP if found
-DNS_SERVER="172.50.120.10" # Replace with DNS Server IP
-INTERNET="172.50.100.10" # Used to access internet for downloading
+WEB_SERVER="172.50.120.20" # Replace with Web Server IP
+INTERNET="0.0.0.0" # Used to access internet for downloading
+DNS_SERVER="0.0.0.0" # Use if needed
 
 # Path to command binaries
-# Still confirm the itegrity of these binaries
-# Verify with "which" command
+# Still confirm the itegrity of these binaries and confirm with "which" command
 CHATTR="/usr/bin/chattr"
 SYSTEMCTL="/usr/bin/systemctl"
 RM="/usr/bin/rm"
@@ -55,20 +53,19 @@ $NFT flush ruleset
 $SYSTEMCTL start firewalld
 
 # Create Custom firewall zones
-# SMB Zone
-$FIREWALL_CMD --permanent --delete-zone=samba >/dev/null 2>&1
-$FIREWALL_CMD --permanent --new-zone=samba
-$FIREWALL_CMD --permanent --zone=samba --add-source=${SMB_SCORING_IP}
-$FIREWALL_CMD --permanent --zone=samba --add-service=samba
-$FIREWALL_CMD --permanent --zone=samba --set-target=ACCEPT
+# DB Scoring Zone
+$FIREWALL_CMD --permanent --delete-zone=db_scoring >/dev/null 2>&1
+$FIREWALL_CMD --permanent --new-zone=db_scoring
+$FIREWALL_CMD --permanent --zone=db_scoring --add-source=${DB_SCORING_IP}
+$FIREWALL_CMD --permanent --zone=db_scoring --add-service=postgresql
+$FIREWALL_CMD --permanent --zone=db_scoring --set-target=ACCEPT
 
-# SSH Zone
-$FIREWALL_CMD --permanent --delete-zone=ssh >/dev/null 2>&1
-$FIREWALL_CMD --permanent --new-zone=ssh
-$FIREWALL_CMD --permanent --zone=ssh --add-source=${SSH_SCORING_IP}
-$FIREWALL_CMD --permanent --zone=ssh --add-service=ssh
-$FIREWALL_CMD --permanent --zone=ssh --add-rich-rule='rule service name="ssh" limit value="5/m" accept'
-$FIREWALL_CMD --permanent --zone=ssh --set-target=ACCEPT
+# Web Zone
+$FIREWALL_CMD --permanent --delete-zone=db_web >/dev/null 2>&1
+$FIREWALL_CMD --permanent --new-zone=db_web
+$FIREWALL_CMD --permanent --zone=db_web --add-source=${WEB_SERVER}
+$FIREWALL_CMD --permanent --zone=db_web --add-service=postgresql
+$FIREWALL_CMD --permanent --zone=db_web --set-target=ACCEPT
 
 # Team Kali
 $FIREWALL_CMD --permanent --delete-zone=mgmt-kali >/dev/null 2>&1
@@ -78,32 +75,22 @@ $FIREWALL_CMD --permanent --zone=mgmt-kali --add-service=ssh
 $FIREWALL_CMD --permanent --zone=mgmt-kali --add-protocol=icmp
 $FIREWALL_CMD --permanent --zone=mgmt-kali --set-target=ACCEPT
 
-# DHCP Server
-$FIREWALL_CMD --permanent --delete-zone=dhcp-trust >/dev/null 2>&1
-$FIREWALL_CMD --permanent --new-zone=dhcp-trust
-$FIREWALL_CMD --permanent --zone=dhcp-trust --add-source=${DHCP_SERVER}
-$FIREWALL_CMD --permanent --zone=dhcp-trust --add-service=dhcp
-$FIREWALL_CMD --permanent --zone=dhcp-trust --set-target=ACCEPT
-
 # Add rule for ICMP if scoring IP needs to ping services
 # $FIREWALL_CMD --permanent --delete-zone=icmp >/dev/null 2>&1
 # $FIREWALL_CMD --permanent --new-zone=icmp
-# $FIREWALL_CMD --permanent --zone=icmp --add-source=${SMB_SOCRING_IP}
-# $FIREWALL_CMD --permanent --zone=icmp --add-source=${SSH_SOCRING_IP}
+# $FIREWALL_CMD --permanent --zone=icmp --add-source=${WEB_SOCRING_IP}
 # $FIREWALL_CMD --permanent --zone=icmp --add-protocol=icmp
 
-# Ignore unknown IPs unless dhcp
+# Ignore unknown IPs
 # Allow all IPs through scoring services with 0.0.0.0
 $FIREWALL_CMD --permanent --zone=drop --add-interface=${INTERFACE}
-$FIREWALL_CMD --permanent --zone=drop --add-service=dhcp
 
 # Direct rules
 $FIREWALL_CMD --permanent --direct --add-rule ipv4 filter OUTPUT 0 -m state --state INVALID -j DROP
 $FIREWALL_CMD --permanent --direct --add-rule ipv4 filter OUTPUT 1 -m state --state ESTABLISHED,RELATED -j ACCEPT
-$FIREWALL_CMD --permanent --direct --add-rule ipv4 filter OUTPUT 2 -p udp -d ${DNS_SERVER} --dport 53 -m length --length 0:4096 -j ACCEPT
-$FIREWALL_CMD --permanent --direct --add-rule ipv4 filter OUTPUT 2 -p tcp -d ${DNS_SERVER} --dport 53 -j ACCEPT
-$FIREWALL_CMD --permanent --direct --add-rule ipv4 filter OUTPUT 2 -p tcp -d ${INTERNET} -m multiport --dports 80,443 -j ACCEPT
-$FIREWALL_CMD --permanent --direct --add-rule ipv4 filter OUTPUT 2 -p udp --dport 67:68 -j ACCEPT
+# $FIREWALL_CMD --permanent --direct --add-rule ipv4 filter OUTPUT 2 -p udp -d ${DNS_SERVER} --dport 53 -m length --length 0:4096 -j ACCEPT
+# $FIREWALL_CMD --permanent --direct --add-rule ipv4 filter OUTPUT 2 -p tcp -d ${DNS_SERVER} --dport 53 -j ACCEPT
+# $FIREWALL_CMD --permanent --direct --add-rule ipv4 filter OUTPUT 2 -p tcp -d ${INTERNET} -m multiport --dports 80,443 -j ACCEPT
 $FIREWALL_CMD --permanent --direct --add-rule ipv4 filter OUTPUT 254 -j LOG --log-prefix "REVERSE_SHELL_ATTEMPT: "
 $FIREWALL_CMD --permanent --direct --add-rule ipv4 filter OUTPUT 255 -j DROP
 
--- scripts/firewall/dns-firewall.sh
+++ scripts/firewall/dns-firewall.sh
@@ -3,22 +3,20 @@
 # This script will avoid using aliases
 # Script must be run as root
 if [ "$EUID" -ne 0 ]; then 
-  echo "Rquires Root: sudo ./shell-firewall.sh"
+  echo "Rquires Root: sudo ./dns-firewall.sh"
   exit
 fi
 
 # Variables
-INTERFACE="en0" # Replace with Interface of server
-SSH_SCORING_IP="0.0.0.0" # Replace with SSH scoring IP if found
-SMB_SCORING_IP="0.0.0.0" # Replace with SMB socring IP if found
+# Replace text in quotes with string
+INTERFACE="en0" # Replace with interface of server
+DNS_SCORING_IP="0.0.0.0" # Replace with DNS scoring IP if found
 EXTERNAL_KALI="192.168.24.20" # Replace with IP of any Kali machine used for remote access
-DHCP_SERVER="0.0.0.0" # Replace with DHCP Server IP if found
-DNS_SERVER="172.50.120.10" # Replace with DNS Server IP
-INTERNET="172.50.100.10" # Used to access internet for downloading
+INTERNET="0.0.0.0" # Used to access the internet for downloading
+PUBLIC_DNS="8.8.8.8" # Google DNS server
 
 # Path to command binaries
-# Still confirm the itegrity of these binaries
-# Verify with "which" command
+# Still confirm the itegrity of these binaries and verify with "which" command
 CHATTR="/usr/bin/chattr"
 SYSTEMCTL="/usr/bin/systemctl"
 RM="/usr/bin/rm"
@@ -55,20 +53,12 @@ $NFT flush ruleset
 $SYSTEMCTL start firewalld
 
 # Create Custom firewall zones
-# SMB Zone
-$FIREWALL_CMD --permanent --delete-zone=samba >/dev/null 2>&1
-$FIREWALL_CMD --permanent --new-zone=samba
-$FIREWALL_CMD --permanent --zone=samba --add-source=${SMB_SCORING_IP}
-$FIREWALL_CMD --permanent --zone=samba --add-service=samba
-$FIREWALL_CMD --permanent --zone=samba --set-target=ACCEPT
-
-# SSH Zone
-$FIREWALL_CMD --permanent --delete-zone=ssh >/dev/null 2>&1
-$FIREWALL_CMD --permanent --new-zone=ssh
-$FIREWALL_CMD --permanent --zone=ssh --add-source=${SSH_SCORING_IP}
-$FIREWALL_CMD --permanent --zone=ssh --add-service=ssh
-$FIREWALL_CMD --permanent --zone=ssh --add-rich-rule='rule service name="ssh" limit value="5/m" accept'
-$FIREWALL_CMD --permanent --zone=ssh --set-target=ACCEPT
+# DNS Zone
+$FIREWALL_CMD --permanent --delete-zone=dns_scoring >/dev/null 2>&1
+$FIREWALL_CMD --permanent --new-zone=dns_scoring
+$FIREWALL_CMD --permanent --zone=dns_scoring --add-source=${DNS_SCORING_IP}
+$FIREWALL_CMD --permanent --zone=dns_scoring --add-service=dns
+$FIREWALL_CMD --permanent --zone=dns_scoring --set-target=ACCEPT
 
 # Team Kali
 $FIREWALL_CMD --permanent --delete-zone=mgmt-kali >/dev/null 2>&1
@@ -78,32 +68,23 @@ $FIREWALL_CMD --permanent --zone=mgmt-kali --add-service=ssh
 $FIREWALL_CMD --permanent --zone=mgmt-kali --add-protocol=icmp
 $FIREWALL_CMD --permanent --zone=mgmt-kali --set-target=ACCEPT
 
-# DHCP Server
-$FIREWALL_CMD --permanent --delete-zone=dhcp-trust >/dev/null 2>&1
-$FIREWALL_CMD --permanent --new-zone=dhcp-trust
-$FIREWALL_CMD --permanent --zone=dhcp-trust --add-source=${DHCP_SERVER}
-$FIREWALL_CMD --permanent --zone=dhcp-trust --add-service=dhcp
-$FIREWALL_CMD --permanent --zone=dhcp-trust --set-target=ACCEPT
-
 # Add rule for ICMP if scoring IP needs to ping services
 # $FIREWALL_CMD --permanent --delete-zone=icmp >/dev/null 2>&1
 # $FIREWALL_CMD --permanent --new-zone=icmp
-# $FIREWALL_CMD --permanent --zone=icmp --add-source=${SMB_SOCRING_IP}
-# $FIREWALL_CMD --permanent --zone=icmp --add-source=${SSH_SOCRING_IP}
+# $FIREWALL_CMD --permanent --zone=icmp --add-source=${DNS_SOCRING_IP}
 # $FIREWALL_CMD --permanent --zone=icmp --add-protocol=icmp
 
-# Ignore unknown IPs unless dhcp
+# Ignore unknown IPs that are not needed
 # Allow all IPs through scoring services with 0.0.0.0
 $FIREWALL_CMD --permanent --zone=drop --add-interface=${INTERFACE}
-$FIREWALL_CMD --permanent --zone=drop --add-service=dhcp
 
 # Direct rules
+# Set otubound dns to 0.0.0.0 or INTERNET if server needs more than google dns
 $FIREWALL_CMD --permanent --direct --add-rule ipv4 filter OUTPUT 0 -m state --state INVALID -j DROP
 $FIREWALL_CMD --permanent --direct --add-rule ipv4 filter OUTPUT 1 -m state --state ESTABLISHED,RELATED -j ACCEPT
-$FIREWALL_CMD --permanent --direct --add-rule ipv4 filter OUTPUT 2 -p udp -d ${DNS_SERVER} --dport 53 -m length --length 0:4096 -j ACCEPT
-$FIREWALL_CMD --permanent --direct --add-rule ipv4 filter OUTPUT 2 -p tcp -d ${DNS_SERVER} --dport 53 -j ACCEPT
+$FIREWALL_CMD --permanent --direct --add-rule ipv4 filter OUTPUT 2 -p udp -d ${PUBLIC_DNS} --dport 53 -m length --length 0:4096 -j ACCEPT
+$FIREWALL_CMD --permanent --direct --add-rule ipv4 filter OUTPUT 2 -p tcp -d ${PUBLIC_DNS} --dport 53 -j ACCEPT
 $FIREWALL_CMD --permanent --direct --add-rule ipv4 filter OUTPUT 2 -p tcp -d ${INTERNET} -m multiport --dports 80,443 -j ACCEPT
-$FIREWALL_CMD --permanent --direct --add-rule ipv4 filter OUTPUT 2 -p udp --dport 67:68 -j ACCEPT
 $FIREWALL_CMD --permanent --direct --add-rule ipv4 filter OUTPUT 254 -j LOG --log-prefix "REVERSE_SHELL_ATTEMPT: "
 $FIREWALL_CMD --permanent --direct --add-rule ipv4 filter OUTPUT 255 -j DROP
 
